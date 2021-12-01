da1650fdca5c9e7f2c76b20cdd8a42f6e3a78081
Sange Sherpa <hellosanges@gmail.com>
2021-12-01 21:14:54 +0545

    gulp included and more

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,2 @@
-//slider initialization
-$(".home-slider").owlCarousel({
-  loop: true,
-  autoplay: true,
-  margin: 10,
-  autoplayHoverPause: true,
-  items: 1,
-  animateOut: "fadeOut",
-  animateIn: "fadeIn",
-});
-
-//hamburger menu
-const btnHamburger = document.querySelector("#btnHamburger");
-btnHamburger.addEventListener("click", function () {
-  console.log("test");
-  if (btnHamburger.classList.contains("open")) {
-    btnHamburger.classList.remove("open");
-  } else {
-    btnHamburger.classList.add("open");
-  }
-});
-
+$(".home-slider").owlCarousel({loop:!0,autoplay:!0,margin:10,autoplayHoverPause:!0,items:1,animateOut:"fadeOut",animateIn:"fadeIn"});const btnHamburger=document.querySelector("#btnHamburger");btnHamburger.addEventListener("click",(function(){console.log("test"),btnHamburger.classList.contains("open")?btnHamburger.classList.remove("open"):btnHamburger.classList.add("open")}));
+//# sourceMappingURL=scripts.js.map
